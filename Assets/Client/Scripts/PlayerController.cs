8c7d05f100a590b7658a496f5ed551cd121dd295
Nastia <adruts@mail.ru>
2024-09-24 13:05:50 +0300

    https://github.com/anastyadru/Game2D.0.git
    Отредактировала скрипт PlayerController

--- Assets/Client/Scripts/PlayerController.cs
+++ Assets/Client/Scripts/PlayerController.cs
@@ -31,7 +31,7 @@ public class PlayerController : MonoBehaviour
         
         MovePlayer();
         
-        if (Input.GetKeyDown(KeyCode.Space) && Mathf.Abs(rb.velocity.y) < 0.05f)
+        if (Input.GetKeyDown(KeyCode.Space) && IsGrounded())
         {
             rb.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
         }
